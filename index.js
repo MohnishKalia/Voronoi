eeaad9014b4bc0c6bb7b5d429361ab8af1917816
Mohnish Kalia <mohnish.kalia@gmail.com>
2022-08-21 21:40:39 -0500

    Adjust width and height

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { writeFile } from "fs/promises";
 import readline from "readline/promises";
 
 const WIDTH = 800;
-const HEIGHT = 800;
+const HEIGHT = 600;
 const COMPONENT_MAX = 255;
 const OUTPUT_FILEPATH = 'output.ppm';
 
@@ -12,8 +12,8 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
  * @typedef {{r:number, g:number, b:number}} Pixel
  * @type {Pixel[][]}
  */
-const pixels = Array(WIDTH).fill(
-    Array(HEIGHT).fill(
+const pixels = Array(HEIGHT).fill(
+    Array(WIDTH).fill(
         { r: 0, g: 0, b: 100 }
     )
 );
@@ -22,6 +22,12 @@ function randomPoint() {
     const x = Math.floor(Math.random() * WIDTH);
     const y = Math.floor(Math.random() * HEIGHT);
     return { x, y };
+}
+
+function dist2(x1, y1, x2, y2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return dx * dx + dy * dy;
 }
 
 async function seedSetup() {
@@ -36,19 +42,30 @@ async function seedSetup() {
     }
 }
 
+function colorDiagram() {
+    for (let y = 0; y < HEIGHT; y++) {
+        for (let x = 0; x < WIDTH; x++) {
+            const px = pixels[y][x];
+        }
+    }
+}
+
 async function savePixels() {
     let fileContent = `P3 ${WIDTH} ${HEIGHT} ${COMPONENT_MAX}\n`;
+
     for (let y = 0; y < HEIGHT; y++) {
-        for (let x = 0; x < HEIGHT; x++) {
+        for (let x = 0; x < WIDTH; x++) {
             const px = pixels[y][x];
             fileContent += `${px.r} ${px.g} ${px.b}\n`;
         }
     }
-    writeFile(OUTPUT_FILEPATH, fileContent);
+
+    await writeFile(OUTPUT_FILEPATH, fileContent);
 }
 
 async function main() {
     await seedSetup();
+    colorDiagram();
     await savePixels();
 }
 
